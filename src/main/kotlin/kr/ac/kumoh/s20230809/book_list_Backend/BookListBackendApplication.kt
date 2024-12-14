4e1768bcbb50c5e42a18736c3906a9e18c657603
singsingi <a91364090@gmail.com>
2024-12-14 19:12:48 +0900

    🎉 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20230809/book_list_Backend/BookListBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230809.book_list_Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookListBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookListBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230809/book_list_Backend/BookListBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230809.book_list_Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookListBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
